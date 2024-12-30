6bd9bafc23326114fac6f2b1e3d1a4cc2f459ef3
0x24dazzle <0x24phys@gmail.com>
2024-12-30 18:55:03 -0500

    output file tls removed

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,3 @@ output "public_ip_address" {
   value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
 }
 
-output "tls_private_key" {
-  value     = tls_private_key.secureadmin_ssh.private_key_pem
-  sensitive = true
