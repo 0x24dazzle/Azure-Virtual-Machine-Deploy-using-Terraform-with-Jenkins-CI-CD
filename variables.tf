5e5628e11b0679fe1581ec4185f1ed2bc2a7a927
0x24dazzle <0x24phys@gmail.com>
2024-12-30 21:38:32 -0500

    variables file updated and tenant, subs ID are now explicitly declared

--- variables.tf
+++ variables.tf
@@ -17,4 +17,25 @@ variable "admin_password" {
   description = "Admin password for the VM"
   default     = "P@ssw0rd1234!"
 }
+variable "azure_subscription_id" {
+  description = "The Azure subscription ID"
+  type        = string
+}
+
+variable "azure_client_id" {
+  description = "The client ID of the Azure service principal"
+  type        = string
+}
+
+variable "azure_client_secret" {
+  description = "The client secret of the Azure service principal"
+  type        = string
+  sensitive   = true
+}
+
+variable "azure_tenant_id" {
+  description = "The Azure tenant ID"
+  type        = string
+}
+
 
